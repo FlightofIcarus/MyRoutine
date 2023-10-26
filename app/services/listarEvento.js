c5e281883d24571dd7d8d6905dc903d1763b1449
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2023-10-26 20:31:33 -0300

    feature:busca eventos no GCalendar e lista

---
+++ app/services/listarEvento.js
@@ -0,0 +1,27 @@
+const {google} = require('googleapis');
+
+
+
+async function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  const res = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: new Date().toISOString(),
+    maxResults: 5,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.error('Nenhum evento encontrado.');
+    return;
+  }
+  console.error('Mostrando seus próximos 05 eventos:');
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    console.log(`${start} - ${event.summary} - ${event.iCalUID}`);
+  });
+}
+
+
+module.exports = listEvents;
