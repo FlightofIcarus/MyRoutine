f324322594b3bca4b73068cdb66ead2683530607
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2023-10-26 20:19:19 -0300

    feature: salva token de autenticação

---
+++ app/controllers/salvarCredenciais.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+
+const CREDENTIALS_PATH = path.join(process.cwd(), '../', 'db', 'credentials.json');
+
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+      type: 'authorized_user',
+      client_id: key.client_id,
+      client_secret: key.client_secret,
+      refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+  }
+
+  module.exports = saveCredentials;
